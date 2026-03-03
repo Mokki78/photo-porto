6e508f2a8bb72f52d2cfde72a1d86ad004c191b7
Mokki78 <100276996+Mokki78@users.noreply.github.com>
2026-03-03 10:22:12 +0100

    feat: remove contact form due to errors

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer() {
         </svg>
       </div>
       <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
-      <ContactForm />
+      
           
       </div>
     </section>
